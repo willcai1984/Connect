a4f49a81b46c5f90ba82c2d023d5a163cd357ef6
willcai1984 <willcai1984@gmail.com>
2014-12-17 11:39:46 +0800

    update

--- Connect/views.py
+++ Connect/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse
 import re, os, simplejson
 def connect_config(request):
