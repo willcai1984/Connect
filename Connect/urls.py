8370ec4a53496a18221465075a6f2588fbe76d32
willcai1984 <willcai1984@gmail.com>
2014-12-18 22:04:26 +0800

    Add # -*- coding: utf-8 -*-

--- Connect/urls.py
+++ Connect/urls.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from Connect.views import connect_config, connect_process, connect_process_longpull, connect_success, connect_fail
--- Connect/views.py
+++ Connect/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*- 
+
 from django.shortcuts import render_to_response
 from django.template.loader import get_template
 from django.template import Context
