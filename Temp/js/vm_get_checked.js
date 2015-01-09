1bb79e7a3580af40daf49be6e690168d49f60d75
willcai1984 <willcai1984@gmail.com>
2015-01-09 17:05:24 +0800

    Add JS

--- Temp/js/vm_get_checked.js
+++ Temp/js/vm_get_checked.js
@@ -1,5 +1,6 @@
-function get_checked_id(){
+function vm_get_checked(){
 	var id_list=new Array()
+	//add name vmlist to list part,to remove title part checkbox from the id list
 	$(":checkbox[name='vmlist']:checked").each(function() {
 		var id = $(this).attr("id");
 		id_list.push(id);
