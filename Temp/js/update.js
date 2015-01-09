ee01a4bcd3979ae1f0dd90bc923444fa0eb9f437
willcai1984 <willcai1984@gmail.com>
2015-01-09 11:39:19 +0800

    update js

--- Temp/js/update.js
+++ Temp/js/update.js
@@ -33,10 +33,14 @@ var updater = {
 				var reg=reg_list[i];
 				var power=power_list[i];
 				var h1=html1.replace(/__id__/g,'check'+i);
-				var h2=html2.replace(/__dis__/g,dis);
-				var h3=html3.replace(/__power__/g,power);
-				var h4=html4.replace(/__vmid__/g,vmid);
-				var h5=html5.replace(/__reg__/g,reg);
+				var h2=html2.replace(/__id__/g,'dis'+i);
+				var h2=h2.replace(/__dis__/g,dis);
+				var h3=html3.replace(/__id__/g,'power'+i);
+				var h3=h3.replace(/__power__/g,power);
+				var h4=html4.replace(/__id__/g,'vmid'+i);
+				var h4=h4.replace(/__vmid__/g,vmid);
+				var h5=html5.replace(/__id__/g,'reg'+i);
+				var h5=h5.replace(/__reg__/g,reg);
 				var h=h+h1+h2+h3+h4+h5;
 			}
 			console.log("Append html is:"+h);
