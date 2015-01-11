c44f16e582ff6d98b9f52e06e61bb0f11d4c5635
willcai1984 <willcai1984@gmail.com>
2015-01-11 22:33:25 +0800

    update

--- Temp/js/vm_power_update.js
+++ Temp/js/vm_power_update.js
@@ -25,11 +25,11 @@ var vm_power_updater = {
 					{
 					if (power_action=='on')
 						{
-						$("#"+power_id).text()='1';
+						$("#"+power_id).text('1');
 						}
 					else if (power_action=='off')
 						{
-						$("#"+power_id).text()='0';
+						$("#"+power_id).text('0');
 						}
 					}
 		}
